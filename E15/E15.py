8f44952a32797327298fd4b7eb97cb67304867b3
Melissa-Mendieta <83670954+Melissa-Mendieta@users.noreply.github.com>
2021-11-14 00:11:07 -0600

    Add files via upload

---
+++ E15/E15.py
@@ -0,0 +1,36 @@
+import webbrowser
+import pyautogui,time
+webbrowser.open("https://forms.office.com/r/S8Jy6Jsvmh", new=2, autoraise=True)
+time.sleep(5)
+pyautogui.click(x=350, y = 479, clicks = 1)
+pyautogui.click(x=442, y = 715, clicks = 1)
+pyautogui.typewrite("Mi unica relacion toxica es con mi carrera")
+pyautogui.press('tab')
+pyautogui.click(x=429, y = 354, clicks = 1)
+time.sleep(4)
+pyautogui.click(x=502, y = 437, clicks = 1)
+pyautogui.click(x=466, y = 588, clicks = 2)
+pyautogui.typewrite("Melissa")
+pyautogui.hotkey('ctrl','alt','q')#@
+pyautogui.typewrite("falso.com")
+pyautogui.press('tab')
+pyautogui.click(x=409, y = 695, clicks = 1)
+time.sleep(3)
+webbrowser.open("https://forms.office.com/r/S8Jy6Jsvmh", new=2, autoraise=True)
+time.sleep(5)
+pyautogui.click(x=371, y = 300, clicks = 1)
+pyautogui.click(x=442, y = 715, clicks = 1)
+pyautogui.typewrite("Un gran poder conlleva una gran responsabilidad")
+pyautogui.press('tab')
+pyautogui.click(x=429, y = 354, clicks = 1)
+time.sleep(4)
+pyautogui.click(x=424, y = 336, clicks = 1)
+pyautogui.click(x=466, y = 588, clicks = 2)
+pyautogui.typewrite("fake")
+pyautogui.hotkey('ctrl','alt','q')#@
+pyautogui.typewrite("falso.com")
+pyautogui.press('tab')
+pyautogui.click(x=409, y = 695, clicks = 1)
+    
+    
+    
